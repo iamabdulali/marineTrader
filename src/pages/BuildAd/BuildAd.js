38d7127f62aa22cb434da56788213a543447ecfb
Abdul Ali <abdulqayyum354@gmail.com>
2024-01-28 22:22:51 +0500

    commit

--- src/pages/BuildAd/BuildAd.js
+++ src/pages/BuildAd/BuildAd.js
@@ -15,7 +15,7 @@ import {
 import { AuthContext } from "../../Context/AuthContext";
 
 const BuildAd = () => {
-  const [step, setStep] = useState(4);
+  const [step, setStep] = useState(1);
 
   const [selectedCategory, setSelectedCategory] = useState("Jet Skis");
   const stepLabels = [
