3074a8f8b7a55399e7e96257826d0f65526aeae6
iamabdulali <abdulqayyum354@gmail.com>
2024-04-26 12:24:16 +0500

    commit

--- src/components/SpotLightListings/SpotLightListings.js
+++ src/components/SpotLightListings/SpotLightListings.js
@@ -43,7 +43,7 @@ const SpotLightListings = ({
               src={image}
               className="w-full lg:max-h-[250px] lg:min-h-[250px] object-cover max-h-[250px] min-h-[250px]"
             />
-            <FaHeart className="absolute right-4 top-4" size={20} />
+            {/* <FaHeart className="absolute right-4 top-4" size={20} /> */}
             {advertID == "3" ? (
               <img src={featuredImage} className="absolute w-28 top-3" />
             ) : (
