c1ebf217586a42dac29a43f6c0cc660bd896707b
iamabdulali <abdulqayyum354@gmail.com>
2024-05-07 16:08:38 +0500

    commit

--- src/components/Payment/StripePaymentForm.js
+++ src/components/Payment/StripePaymentForm.js
@@ -227,7 +227,7 @@ const StripePaymentForm = ({
             ""
           )}
           {hasFeaturedBundle != 0 && advertType == "Featured" ? (
-            <div className="bg-[#1C5DBF] text-white p-6 pt-0 shadow-[7px]">
+            <div className="bg-[#1C5DBF] text-white p-6 shadow-[7px]">
               <p className="sm:text-2xl text-base font-semibold">
                 <img
                   className="inline-block w-10 sm:mr-0 mr-3"
@@ -236,7 +236,7 @@ const StripePaymentForm = ({
                 />{" "}
                 Allowances/Bundles
               </p>
-              <div className="flex items-center sm:text-base text-sm justify-between">
+              <div className="flex items-center sm:text-base text-sm justify-between mt-3">
                 <p>Featured Bundle Balance</p>
                 <p className="font-medium sm:text-lg text-base">
                   {hasFeaturedBundle}
